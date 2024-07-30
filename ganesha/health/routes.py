f192a585860d5bfea1f05aaaa779bf3e9580b5ca
oguz <oguzyesil17@gmail.com>
2024-07-30 14:06:58 +0300

    health route updated for check user in db

--- ganesha/health/routes.py
+++ ganesha/health/routes.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter, status, Security
+from bson import ObjectId
+from fastapi import APIRouter, status, Security, HTTPException
 from fastapi_jwt import JwtAuthorizationCredentials
 from ganesha.auth.login_utilities import access_security
+from ganesha.collections import get_collection, Collections
 from ganesha.models import StatusResponse
 
 router = APIRouter()
@@ -13,4 +15,11 @@ def check_server_health():
 
 @router.get('/check-key', status_code=status.HTTP_200_OK, response_model=StatusResponse)
 def check_key_health(credentials: JwtAuthorizationCredentials = Security(access_security)):
+    user_id = credentials.subject['id']
+    db_check_user = get_collection(Collections.USER_COLLECTION).find_one({'_id': ObjectId(user_id)})
+
+    if not db_check_user:
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED,
+                            detail='there is no user with this id anymore')
+
     return StatusResponse(status=True)
